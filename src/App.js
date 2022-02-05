309a75dbd7d6115d6986e411b46e4b30dff679c6
Suraj Yadav <sy242531@gmail.com>
2022-02-05 10:47:33 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-// import Modal from 'react-modal';
 import InsertData from './Components/InsertData';
 
 
@@ -11,52 +10,24 @@ function App() {
   const [persons, setPersonState] = useState(data);
   const [showModal, setShowModal] = useState(false)
 
-  const openModal = (event)=>{
+  const openModal = (event) => {
     setShowModal(true)
   }
-  const closeModal = (event)=>{
+  const closeModal = (event) => {
     document.getElementById('insertData').reset();
     setShowModal(false)
   }
 
-  // const toggleModal = () => {
-  //   setShowModal(!showModal);
-  // }
 
-  // const handleClose = () => setShowModal(false);
-  // const handleShow = () => setShowModal(true);
 
   console.log(showModal);
 
   return (
     <div>
       <div>
-        <button  className='addBirthDay'  onClick={openModal}>Add More Birthdays</button>
-          <InsertData showModal= {showModal} openModal={openModal} closeModal={closeModal} />
+        <button className='addBirthDay' onClick={openModal}>Add More Birthdays</button>
+        <InsertData showModal={showModal} openModal={openModal} closeModal={closeModal} />
 
-        {/* 
-      ====================
-      one way of adding modal to react *
-      =====================
-      /}
-      {/* 
-        <Modal
-        isOpen={showModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        Hello
-      </Modal> 
-      */}
-
-        {/* 
-      ===================
-      Bootstrap Modal
-      ===================
-      */}
-
-        
       </div>
       <main>
 
